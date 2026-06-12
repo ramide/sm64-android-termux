4d5748badd5480a174550f169d2eb9406c644d80
ramide@users <3691739+ramide@users.noreply.github.com>
2026-06-12 22:59:31 +0200

    Fix: link liblog for __android_log_print in termux.c

--- terminal-emulator/src/main/jni/Android.mk
+++ terminal-emulator/src/main/jni/Android.mk
@@ -2,5 +2,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE:= libtermux
 LOCAL_SRC_FILES:= termux.c
+LOCAL_LDLIBS := -llog
 LOCAL_LDFLAGS := -Wl,-z,max-page-size=16384
 include $(BUILD_SHARED_LIBRARY)
